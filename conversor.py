05be70009b77fc13b8f827504efd53c278f28235
Nicolas Vivas <nicolasvivas93@gmail.com>
2022-10-23 20:16:23 -0300

    Actualizar conversor

--- conversor.py
+++ conversor.py
@@ -1,3 +1,26 @@
+menu = """
+Bienvenido al conversor de monedas
+
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elige una opción:
+"""
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input("¿Cuantos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 275
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2) #redondeamos los decimales a dos dígitos decimales
+    dolares = str(dolares) # convertimos a texto la variable dolares
+
+print("Tienes $" + dolares + " dolares")
+
+
 pesos = input("¿Cuantos pesos argentinos tienes?: ")
 pesos = float(pesos)
 valor_dolar = 275
