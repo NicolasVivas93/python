e80e4e26f2df03fd9649c1b28c7b433ade29b12d
NicolasVivas93 <nicolasvivas93@gmail.com>
2022-10-29 11:40:22 -0300

    Improve code adding a promt and setting exceptions

--- file-uppercase.py
+++ file-uppercase.py
@@ -1,4 +1,11 @@
-fhand = open('mbox-short', 'r')
+file = input("Enter file name:")
+
+try:
+    fhand = open(file, 'r')
+except:
+    print("File not found")
+    quit()
+    
 for line in fhand:
     line = line.rstrip().upper()
     print(line)
