2ac691bf1889c0440fe898295bca788b892a3b30
NicolasVivas93 <nicolasvivas93@gmail.com>
2022-11-08 07:21:47 -0300

    Work with web forms and submits with MechanicalSoup

--- test-mechanical-soup.py
+++ test-mechanical-soup.py
@@ -3,5 +3,7 @@ import mechanicalsoup
 browser = mechanicalsoup.Browser()
 url = "http://olympus.realpython.org/login"
 page = browser.get(url)
+page_html = page.soup  # type: ignore
 
-page.soup
+print(page_html)
+print(page_html.prettify())
