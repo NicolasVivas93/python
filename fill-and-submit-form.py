fb513aebf23aa96bd02e7296f4c59d824d0a0b70
NicolasVivas93 <nicolasvivas93@gmail.com>
2022-11-09 07:30:41 -0300

    Test mechanical with examples

--- fill-and-submit-form.py
+++ fill-and-submit-form.py
@@ -1,15 +1,22 @@
 import mechanicalsoup
 
 browser = mechanicalsoup.Browser()
-url = "http://olympus.realpython.org/login"
+url = "https://crmciec.hostingcrm.com.ar/index.php?module=Users&action=Login"
 login_page = browser.get(url)
 login_html = login_page.soup  # type: ignore
 
-
 form = login_html.select("form")[0]
-form.select("input")[0]["value"] = "zeus"
-form.select("input")[1]["value"] = "ThunderDude"
 
-profiles_page = browser.submit(form, login_page.url)
-print(profiles_page.url)
+usuario = form.select("input[type=text]")
+password = form.select("input[type=password]")
+print(usuario)
+print(password)
+
+
+
+#form.select("input")[0]["value"] = "zeus"
+#form.select("input")[1]["value"] = "ThunderDude"
+
+#profiles_page = browser.submit(form, login_page.url)
+#print(profiles_page.url)
 
