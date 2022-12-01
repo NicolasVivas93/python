f4e37226db2a067c76984997786790906d1e00c2
Nicolas Vivas <nicolasvivas93@gmail.com>
2022-12-01 17:15:11 -0300

    Update desktop automation for Tango

--- automate-desktop.py
+++ automate-desktop.py
@@ -15,4 +15,40 @@ pyautogui.doubleClick()
 time.sleep(3.5)
 pyautogui.press("enter")
 
+time.sleep(3.5)
+pyautogui.press("delete")
+pyautogui.press("enter")
+
+
+#9 veces flecha abajo
+for i in range(9):
+    pyautogui.press("down")
+
+verificar = input("Es correcto el numero de factura? (Y/N):")
+verif_lower = verificar.lower()
+
+if verif_lower == 'n':
+    print("Factura incorrecta")
+elif verif_lower == 'y':
+    print("Factura correcta, volver a Tango")
+    time.sleep(5)
+    pyautogui.press("enter")
+    time.sleep(0.5)
+    pyautogui.press("enter")
+    time.sleep(0.5)
+    pyautogui.press("enter")
+    pyautogui.typewrite("unc-FCEF")
+    pyautogui.press("enter")
+    pyautogui.press("enter")
+    
+    for i in range(5):
+        time.sleep(0.5)
+        pyautogui.press("enter")
+    
+    
+    
+
+
+    
+    
 
